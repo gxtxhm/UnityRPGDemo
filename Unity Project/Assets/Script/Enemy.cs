bd564b0a153e4ccd308ac9a5042d4b07e36c2bf9
gxtxhm <53137918+gxtxhm@users.noreply.github.com>
2022-11-26 10:44:23 +0900

    asd
    sda

---
+++ Unity Project/Assets/Script/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int maxHp;
+    int curHp;
+    Rigidbody rigid;
+    Collider enemyCollider;
+    bool isDamaged = false;
+    void Start()
+    {
+        maxHp = 100;
+        curHp = maxHp;
+        rigid = GetComponent<Rigidbody>();
+        enemyCollider = GetComponent<CapsuleCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (!isDamaged&&other.tag=="Hammer")
+        {
+            //약간 뒤로 충격 + 1초정도 무적 코루틴으로
+            Debug.Log("Hammer에 맞음!");
+            curHp -= other.gameObject.GetComponentInParent<Weapon>().damage;
+            StartCoroutine("Damaged");
+        }
+    }
+
+    IEnumerator Damaged()
+    {
+        yield return new WaitForSeconds(0.1f);
+        isDamaged = true;
+        yield return new WaitForSeconds(0.6f);
+        isDamaged = false;
+
+    }
+}
---
+++ Unity Project/Assets/Script/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int maxHp;
+    int curHp;
+
+    Rigidbody playerRigid;
+    Animator animator;
+
+    public float speed = 3f;
+    float horizontal, vertical;
+    Vector3 moveVec;
+
+    Weapon weapon;
+    public float swingDelay = 0.5f;
+    float lastSwing;
+    void Start()
+    {
+        maxHp = 100;
+        curHp = maxHp;
+        playerRigid = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
+        // 나중에 바꿔야함
+        weapon = GetComponentInChildren<Weapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+    
+    void PlayerMove()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        moveVec = new Vector3(horizontal, 0, vertical).normalized;
+        transform.LookAt(transform.position + moveVec);
+        if((horizontal!=0||vertical!=0)&&Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.SetBool("IsRun", true);
+            transform.position += moveVec *2* speed * Time.deltaTime;
+        }
+        else
+        {
+            animator.SetBool("IsRun", false);
+            transform.position += moveVec * speed * Time.deltaTime;
+        }
+
+        
+        if(horizontal!=0||vertical!=0)
+        {
+            animator.SetBool("IsWalk", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalk", false);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            
+
+            // 
+            if(swingDelay+lastSwing<Time.time)
+            {
+                Debug.Log("공격!");
+                animator.SetTrigger("IsAttack");
+                weapon.UseWeapon();
+                lastSwing = Time.time;
+            }
+
+            
+        }
+    }
+
+}
